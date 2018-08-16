af6e63c179756ffb51f306754710307edd1bef34
deadman96385 <seanhoyt963@gmail.com>
2018-08-16 04:41:42 -0500

    Import C9000ZCU1BQL1

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#3 SMP PREEMPT Thu Jan 18 21:16:16 CST 2018"
+#define LINUX_COMPILE_BY "lilianhua"
+#define LINUX_COMPILE_HOST "ubuntu70-PowerEdge-R710"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
